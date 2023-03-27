822964733aeb3858a048aa4d104573897454ce3b
KeyFicller <799872510@qq.com>
2023-03-28 00:55:38 +0800

    inti

---
+++ premake5.lua
@@ -0,0 +1,33 @@
+project "glad"
+    kind "StaticLib"
+    language "C"
+    staticruntime "on"
+    
+
+    targetdir(g_workspaceDir .. "/bin/" .. g_outputDir .. "/%{prj.name}")
+    objdir(g_workspaceDir .. "/bin-int/" .. g_outputDir .. "/%{prj.name}")
+
+    defines {"_CRT_SECURE_NO_WARNINGS"}
+
+    files
+    {
+        "include/glad/glad.h",
+        "include/KHR/khrplatform.h",
+        "src/glad.c"
+    }
+
+	includedirs
+	{
+		"include"
+	}
+    
+    filter "system:windows"
+        systemversion "latest"
+        
+    filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
